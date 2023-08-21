0c7ae6838e62f18d688911ddac0715d72a0e260b
Under Review <31888083+DVSOD@users.noreply.github.com>
2023-08-21 15:22:09 -0700

    Update rgbd2point.py

--- rgbd2point.py
+++ rgbd2point.py
@@ -16,10 +16,19 @@ plt.imshow(rgbd_image.depth)
 plt.show()
 
 # The RGBD image can be converted into a point cloud, given a set of camera parameters.
+# To be specific, some available camera intrinsic parameters are provided below:
+'''
+RGB-D People: http://www2.informatik.uni-freiburg.de/~spinello/RGBD-dataset.html
+RGB-D Scene: https://rgbd-dataset.cs.washington.edu/dataset/rgbd-scenes-v2/
+PTB: https://tracking.cs.princeton.edu/dataset.html
+CDTB: Calibration parameters will be provided once requested
+DET: Not provided. Because the main authors have left the university. (For reference purposes but not precise: https://www.intelrealsense.com/depth-camera-d415/)
+Tracklam: http://tracklam.informatik.uni-freiburg.de/
+Track3D: Not provided. But the authors directly provide the point cloud in https://github.com/yjybuaa/Track-it-in-3D. 
+'''
 pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image,
-                                                     pinhole_camera_intrinsic
-                                                     # o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
-                                                     )
+                                                     o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+
 # Flip it, otherwise the point cloud will be upside down.
 pcd.transform([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
 
